89ddc689833c036c92dcca62c0b1a22070fa3382
Egito Filho <egitofilho4@gmail.com>
2023-12-19 22:19:12 -0300

    first commit

---
+++ HabitPlus/HabitPlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitPlusApp.swift
+//  HabitPlus
+//
+//  Created by Egito Filho on 19/12/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitPlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            SplashView()
+        }
+    }
+}
---
+++ HabitPlus/Splash/View/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  HabitPlus
+//
+//  Created by Egito Filho on 19/12/23.
+//
+
+import SwiftUI
+
+struct SplashView: View{
+    var body: some View{
+        Text("Olá")
+    }
+}
+
+struct SplashView_Previews: PreviewProvider{
+    static var previews: some View {
+        SplashView()
+    }
+}
