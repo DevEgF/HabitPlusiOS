3e026d0216d7236cf23d7f3efdf4eb314b78d51b
Egito Filho <egitofilho4@gmail.com>
2023-12-19 22:49:21 -0300

    add logo and create function extension

--- HabitPlus/Splash/View/SplashView.swift
+++ HabitPlus/Splash/View/SplashView.swift
@@ -14,13 +14,29 @@ struct SplashView: View {
     var body: some View {
         switch state {
         case .loading:
-            Text("Loading")
+            loadingView()
         case .goToSingInScrenn:
             Text("Carregar tela de login")
         case .goToHomeScrenn:
             Text("Carregar homescreen")
         case .error(let msg):
             Text("Carregar mensagem de erro: \(msg)")
+        }
+    }
+}
+
+/// Criação de funções em extensões
+///*consigo passar parametros*
+extension SplashView {
+    func loadingView() -> some View {
+        ZStack {
+            Image("logo")
+                .resizable()
+                .scaledToFit()
+                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                .padding(20)
+                .background(Color.white)
+                .ignoresSafeArea()
         }
     }
 }
